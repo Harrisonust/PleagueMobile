47c244eefd6bd9c1f82732147fbe955a00f30225
Harrison <sloab@ust.hk>
2022-01-14 15:55:25 +0800

    build mian frag

--- app/src/main/java/com/example/gamechangermobile/MainActivity.kt
+++ app/src/main/java/com/example/gamechangermobile/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.gamechangermobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gamesFrag = GameFragment()
+        val statsFrag = StatsFragment()
+
+        bottom_navigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.games_page -> {
+                    Toast.makeText(this, "game", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.mainFragView, gamesFrag)
+                        commit()
+                    }
+                }
+
+                R.id.stats_page -> {
+                    Toast.makeText(this, "stats", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.mainFragView, statsFrag)
+                        commit()
+                    }
+                }
+            }
+        }
+
+
     }
 }
