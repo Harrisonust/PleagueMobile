cea3b26e90b05faabcfc2ef51b99edb837052a6e
Harrison <sloab@ust.hk>
2022-01-16 12:45:58 +0800

    add players stats

--- app/src/main/java/com/example/gamechangermobile/GameActivity.kt
+++ app/src/main/java/com/example/gamechangermobile/GameActivity.kt
@@ -21,9 +21,8 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
         val game_data = intent.getParcelableExtra<Game>("GAME_DATA")
         if(game_data != null) {
-            Log.d("Debug", game_data!!.hostName)
-            game_page_header_guest_icon.setImageResource(game_data!!.guestImg)
-            game_page_header_host_icon.setImageResource(game_data!!.hostImg)
+            game_page_header_guest_icon.setImageResource(game_data!!.GuestTeam.ProfilePic)
+            game_page_header_host_icon.setImageResource(game_data!!.HostTeam.ProfilePic)
             game_page_header_guest_score.text = game_data!!.guestScore.toString()
             game_page_header_host_score.text = game_data!!.hostScore.toString()
             game_page_header_time.text = game_data!!.remainingTime
@@ -35,13 +34,19 @@ class GameActivity : AppCompatActivity() {
         game_page_tab.addTab(game_page_tab.newTab().setText("Plays"))
 
 //        game_page_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(game_page_tab))
+//        game_page_viewpager.adapter = RosterAdapter(game_data?.)
+//        game_page_viewpager.setCurrentItem(0)
 //        game_page_tab.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(game_page_viewpager))
 
         game_page_header_host_icon.setOnClickListener {
             val intent = Intent(this, TeamActivity::class.java)
-            val team = Team("Lioneers", "Hsinchu", R.drawable.lioneers)
-            team.ranking = "2nd"
-            team.record = "4 - 3"
+            val team = game_data?.HostTeam
+            intent.putExtra("SELECTED_TEAM", team)
+            startActivity(intent)
+        }
+        game_page_header_guest_icon.setOnClickListener {
+            val intent = Intent(this, TeamActivity::class.java)
+            val team = game_data?.GuestTeam
             intent.putExtra("SELECTED_TEAM", team)
             startActivity(intent)
         }
--- app/src/main/java/com/example/gamechangermobile/TeamActivity.kt
+++ app/src/main/java/com/example/gamechangermobile/TeamActivity.kt
@@ -3,9 +3,16 @@ package com.example.gamechangermobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.example.gamechangermobile.models.Player
 import com.example.gamechangermobile.models.Team
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_player.*
 import kotlinx.android.synthetic.main.activity_team.*
 import kotlinx.android.synthetic.main.activity_team.view.*
@@ -35,6 +42,26 @@ class TeamActivity : AppCompatActivity() {
         team_page_tab.addTab(team_page_tab.newTab().setText("SCHEDULE"))
         team_page_tab.addTab(team_page_tab.newTab().setText("ROSTER"))
 
+        team_page_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(team_page_tab))
+        team_page_viewpager.adapter = VPagerAdapter(supportFragmentManager,3, teamData!!)
+        team_page_viewpager.setCurrentItem(0)
+        team_page_tab.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(team_page_viewpager))
+
         //TODO: player stats view
     }
 }
+
+class VPagerAdapter(f: FragmentManager, bh:Int, val team: Team) : FragmentPagerAdapter(f,bh){
+    override fun getCount(): Int = 3
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> TeamPageInfoFragment()
+            1 -> TeamPageScheduleFragment()
+            else -> TeamPageRosterFragment(team)
+        }
+    }
+}
+
+
+
