4bcf321562145a62428a0bb9c816abaef328ac13
Ann <cchouaa@connect.ust.hk>
2022-01-19 17:52:18 +0800

    dynamic table required files init

---
+++ app/src/main/java/com/example/gamechangermobile/views/VerticalScroll.kt
@@ -0,0 +1,34 @@
+package com.example.gamechangermobile.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ScrollView
+
+class VerticalScroll : ScrollView {
+    private var scrollViewListener: ScrollViewListener? = null
+
+    interface ScrollViewListener {
+        fun onScrollChanged(scrollView: VerticalScroll?, x: Int, y: Int, oldx: Int, oldy: Int)
+    }
+
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+
+    fun setScrollViewListener(scrollViewListener: ScrollViewListener?) {
+        this.scrollViewListener = scrollViewListener
+    }
+
+    override fun onScrollChanged(x: Int, y: Int, oldx: Int, oldy: Int) {
+        super.onScrollChanged(x, y, oldx, oldy)
+        if (scrollViewListener != null) {
+            scrollViewListener!!.onScrollChanged(this, x, y, oldx, oldy)
+        }
+    }
+}
