4e9e22b8fe9e430179a27c08875735133ead74ed
Harrison <sloab@ust.hk>
2022-01-27 19:17:16 +0800

    add some functions and format

--- app/src/main/java/com/example/gamechangermobile/models/Player.kt
+++ app/src/main/java/com/example/gamechangermobile/models/Player.kt
@@ -6,61 +6,74 @@ import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
-class Player(val firstName:String="",
-             val lastName:String="",
-             var profilePic: Int= R.drawable.ic_baseline_sports_basketball_24,
-             var stats: MutableMap<Date, PlayerStats> = mutableMapOf<Date, PlayerStats>(),
-             var team: Team = Team("", "", 0),
-             var age:Int = 0,
-             var number:String = "",
-             var position:String = "",
+class Player(
+        val firstName: String = "",
+        val lastName: String = "",
+        var profilePic: Int = R.drawable.ic_baseline_sports_basketball_24,
+        var stats: MutableMap<Date, PlayerStats> = mutableMapOf<Date, PlayerStats>(),
+        var team: Team = Team("", "", 0),
+        var age: Int = 0,
+        var number: String = "",
+        var position: String = "",
 
-             ): Parcelable {
-                var fullName: String = ""
-                    get() {
-                        return "$firstName $lastName"
-                    }
+        ) : Parcelable {
+    var fullName: String = ""
+        get() {
+            return "$firstName $lastName"
+        }
 
-                var abbrName: String = ""
-                    get() {
-                        val abbrFirstName = firstName[0]
-                        return "$abbrFirstName. $lastName"
-                    }
+    var abbrName: String = ""
+        get() {
+            val abbrFirstName = firstName[0]
+            return "$abbrFirstName. $lastName"
+        }
 
-                var gamePlayed: Int = 0
-                    get(){
-                        return stats.size
-                    }
+    var gamePlayed: Int = 0
+        get() {
+            return stats.size
+        }
 
-                var averageStat: PlayerStats = PlayerStats()
-                    get(){
-                        var averagePlayerStats = PlayerStats()
-                        stats.values.forEachIndexed{ _, eachGameStats ->
-                            eachGameStats.data.forEach {
-                                averagePlayerStats.data[it.key] =  averagePlayerStats.data[it.key]!! + eachGameStats.data[it.key]!!
-                            }
-                        }
-
-                        averagePlayerStats.data.forEach {
-                            averagePlayerStats.data[it.key] = averagePlayerStats.data[it.key]!! / gamePlayed
-                        }
-
-                        return averagePlayerStats
-                    }
-
-                fun getStat(date: Date, type: String): Float {
-                    return this.stats[date]?.data?.get(type) ?: 0F
+    var averageStat: PlayerStats = PlayerStats()
+        get() {
+            var averagePlayerStats = PlayerStats()
+            stats.values.forEachIndexed { _, eachGameStats ->
+                eachGameStats.data.forEach {
+                    averagePlayerStats.data[it.key] = averagePlayerStats.data[it.key]!! + eachGameStats.data[it.key]!!
                 }
+            }
 
-                fun getStats(date: Date): PlayerStats{
-                    return stats[date] ?: PlayerStats()
-                }
+            averagePlayerStats.data.forEach {
+                averagePlayerStats.data[it.key] = averagePlayerStats.data[it.key]!! / gamePlayed
+            }
 
-                fun setStats(date: Date, type: String, value: Float){
-                    this.stats[date]?.data?.set(type, value)
-                }
+            return averagePlayerStats
+        }
 
-                fun setStats(date: Date, stats: PlayerStats){
-                    this.stats[date] = stats
+    var accumulatedStats: PlayerStats = PlayerStats()
+        get() {
+            var averagePlayerStats = PlayerStats()
+            stats.values.forEachIndexed { _, eachGameStats ->
+                eachGameStats.data.forEach {
+                    averagePlayerStats.data[it.key] = averagePlayerStats.data[it.key]!! + eachGameStats.data[it.key]!!
                 }
-             }
+            }
+
+            return averagePlayerStats
+        }
+
+    fun getStat(date: Date, type: String): Float {
+        return this.stats[date]?.data?.get(type) ?: 0F
+    }
+
+    fun getStats(date: Date): PlayerStats {
+        return stats[date] ?: PlayerStats()
+    }
+
+    fun setStats(date: Date, type: String, value: Float) {
+        this.stats[date]?.data?.set(type, value)
+    }
+
+    fun setStats(date: Date, stats: PlayerStats) {
+        this.stats[date] = stats
+    }
+}
