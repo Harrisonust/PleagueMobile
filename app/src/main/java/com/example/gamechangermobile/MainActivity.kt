d32456182aed13c359a172f06358048af7b4bfbb
Harrison <sloab@ust.hk>
2022-04-23 19:19:46 +0800

    make fetching games logic an asyncTask class

--- app/src/main/java/com/example/gamechangermobile/MainActivity.kt
+++ app/src/main/java/com/example/gamechangermobile/MainActivity.kt
@@ -1,31 +1,20 @@
 package com.example.gamechangermobile
 
+import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.gamechangermobile.database.GCPlayerInfoWithBox
-import com.example.gamechangermobile.database.GCStatsParser
-import com.example.gamechangermobile.database.GCTeam
 import com.example.gamechangermobile.models.*
-import com.example.gamechangermobile.network.Api
-import com.example.gamechangermobile.network.UrlRequestCallback
-import kotlinx.android.synthetic.main.activity_game.*
 import kotlinx.android.synthetic.main.activity_main.*
-import org.chromium.net.CronetEngine
-import org.chromium.net.UrlRequest
 import org.jsoup.Jsoup
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    //    @RequiresApi(Build.VERSION_CODES.N)
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -54,47 +43,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        Thread {
-            val doc =
-                Jsoup.connect("https://pleagueofficial.com/schedule-regular-season/2021-22").get()
-            doc.select("div.col-lg-12.col-12")
-                .parallelStream()
-                .filter { it != null }
-                .forEach {
-                    val regex =
-                        "^([0-9][0-9])/([0-9][0-9]) \\(.*?\\) ([0-9][0-9]:[0-9][0-9]) 客隊 (\\S+) (.*?) ([0-9]*?) [0-9]*? G([0-9][0-9]) (.*?) 追蹤賽事 (.*? / .*?) ([0-9]*?) [0-9]*? 主隊 (\\S+) (.*?) 數據 售票 (.*? / .*?)\$".toRegex()
-                    val parsed = regex.find(it.text())
-                    val month = parsed?.groups?.get(1)?.value
-                    val date = parsed?.groups?.get(2)?.value
-                    val time = parsed?.groups?.get(3)?.value
-                    val guest = parsed?.groups?.get(5)?.value
-                    val guestScore = parsed?.groups?.get(6)?.value
-                    val id = parsed?.groups?.get(7)?.value
-                    val location = parsed?.groups?.get(8)?.value
-                    val audience = parsed?.groups?.get(9)?.value
-                    val hostScore = parsed?.groups?.get(10)?.value
-                    val host = parsed?.groups?.get(12)?.value
-                    var game = Game(
-                        gameId = GameID(id!!.toInt() + 72),
-                        date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse("2022-$month-${date}T${time}:00Z"),
-                        guestTeam = getTeamIdByName(guest!!),
-                        hostTeam = getTeamIdByName(host!!),
-                        guestScore = guestScore!!.toInt(),
-                        hostScore = hostScore!!.toInt(),
-                    )
-                    val today = Date()
-                    if (today.compareTo(game.date) > 0)
-                        game.status = GameStatus.END
-                    else
-                        game.status = GameStatus.NOT_YET_START
-                    games.add(game)
-                    getTeamById(game.hostTeam)?.gamesIdList?.add(game.gameId)
-                    getTeamById(game.guestTeam)?.gamesIdList?.add(game.gameId)
-
-                }
-////                Only the original thread that created a view hierarchy can touch its views.
-//                gamesFrag.updateGameCardView()
-        }.start()
+        FetchGamesTask().execute()
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -168,5 +117,54 @@ class MainActivity : AppCompatActivity() {
 
         val games: MutableSet<Game> = mutableSetOf<Game>()
 
+    }
+}
+
+
+class FetchGamesTask : AsyncTask<Unit, Int, Boolean>() {
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun doInBackground(vararg p0: Unit?): Boolean = try {
+        val doc = Jsoup.connect("https://pleagueofficial.com/schedule-regular-season/2021-22").get()
+        doc.select("div.col-lg-12.col-12")
+            .parallelStream()
+            .filter { it != null }
+            .forEach {
+                val regex =
+                    "^([0-9][0-9])/([0-9][0-9]) \\(.*?\\) ([0-9][0-9]:[0-9][0-9]) 客隊 (\\S+) (.*?) ([0-9]*?) [0-9]*? G([0-9][0-9]) (.*?) 追蹤賽事 (.*? / .*?) ([0-9]*?) [0-9]*? 主隊 (\\S+) (.*?) 數據 售票 (.*? / .*?)\$".toRegex()
+                val parsed = regex.find(it.text())
+                val month = parsed?.groups?.get(1)?.value
+                val date = parsed?.groups?.get(2)?.value
+                val time = parsed?.groups?.get(3)?.value
+                val guest = parsed?.groups?.get(5)?.value
+                val guestScore = parsed?.groups?.get(6)?.value
+                val id = parsed?.groups?.get(7)?.value
+                val location = parsed?.groups?.get(8)?.value
+                val audience = parsed?.groups?.get(9)?.value
+                val hostScore = parsed?.groups?.get(10)?.value
+                val host = parsed?.groups?.get(12)?.value
+                var game = Game(
+                    gameId = GameID(id!!.toInt() + 72),
+                    date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse("2022-$month-${date}T${time}:00Z"),
+                    guestTeam = getTeamIdByName(guest!!),
+                    hostTeam = getTeamIdByName(host!!),
+                    guestScore = guestScore!!.toInt(),
+                    hostScore = hostScore!!.toInt(),
+                )
+                val today = Date()
+                if (today.compareTo(game.date) > 0)
+                    game.status = GameStatus.END
+                else
+                    game.status = GameStatus.NOT_YET_START
+                MainActivity.games.add(game)
+                getTeamById(game.hostTeam)?.gamesIdList?.add(game.gameId)
+                getTeamById(game.guestTeam)?.gamesIdList?.add(game.gameId)
+
+            }
+////                Only the original thread that created a view hierarchy can touch its views.
+//                gamesFrag.updateGameCardView()
+
+        true
+    } catch (e: Exception) {
+        false
     }
 }
