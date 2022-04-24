4f36395a229c73e210b1632ca4ff282376137368
Ann <cchouaa@connect.ust.hk>
2022-04-24 11:52:28 +0800

    okHttp class added

---
+++ app/src/main/java/com/example/gamechangermobile/network/OkHttp.kt
@@ -0,0 +1,37 @@
+package com.example.gamechangermobile.network
+
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
+
+class OkHttp {
+    var delegate: OnSuccessResponse? = null
+    constructor(onSuccessResponse: OnSuccessResponse) {
+        delegate = onSuccessResponse
+    }
+    interface OnSuccessResponse {
+        fun action(result: String?)
+    }
+    fun getRequest(path: String, queryParams: Map<String, String>, source: String) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(Api.url(path, queryParams, source))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    delegate?.action(response.body!!.string())
+                }
+            }
+        })
+    }
+
+
+}
