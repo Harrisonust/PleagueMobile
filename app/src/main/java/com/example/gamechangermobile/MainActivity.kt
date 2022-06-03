0308cd6c4104681c48dc91dbb5cec29e7816bed1
Harrison <sloab@ust.hk>
2022-06-03 15:41:16 +0800

    fix bugs of game card
    previously only regular season's games are parsed correctly, now
    all games, including playoffs and final are properly handled.

--- app/src/main/java/com/example/gamechangermobile/MainActivity.kt
+++ app/src/main/java/com/example/gamechangermobile/MainActivity.kt
@@ -124,46 +124,54 @@ class MainActivity : AppCompatActivity() {
     inner class FetchGamesTask : AsyncTask<Unit, Int, Boolean>() {
         @RequiresApi(Build.VERSION_CODES.N)
         override fun doInBackground(vararg p0: Unit?): Boolean = try {
-            val doc =
-                Jsoup.connect("https://pleagueofficial.com/schedule-regular-season/2021-22").get()
-            doc.select("div.col-lg-12.col-12")
-                .parallelStream()
-                .filter { it != null }
-                .forEach {
-                    val regex =
-                        "^([0-9][0-9])/([0-9][0-9]) \\(.*?\\) ([0-9][0-9]:[0-9][0-9]) 客隊 (?:\\S+) (.*?) ([0-9]*?) [0-9]*? G([0-9][0-9]) (.*?) 追蹤賽事 (.*? / .*?) ([0-9]*?) [0-9]*? 主隊 (?:\\S+) (.*?) 數據 售票 (?:.*? / .*?)\$".toRegex()
-                    val parsed = regex.find(it.text())
-                    val month = parsed?.groups?.get(1)?.value
-                    val date = parsed?.groups?.get(2)?.value
-                    val time = parsed?.groups?.get(3)?.value
-                    val guest = parsed?.groups?.get(4)?.value
-                    val guestScore = parsed?.groups?.get(5)?.value
-                    val id = parsed?.groups?.get(6)?.value
-                    val location = parsed?.groups?.get(7)?.value
-                    val audience = parsed?.groups?.get(8)?.value
-                    val hostScore = parsed?.groups?.get(9)?.value
-                    val host = parsed?.groups?.get(10)?.value
-                    var game = Game(
-                        gameId = GameID(id!!.toInt() + 72),
-                        date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse("2022-$month-${date}T${time}:00Z"),
-                        guestTeam = getTeamIdByName(guest!!),
-                        hostTeam = getTeamIdByName(host!!),
-                        guestScore = guestScore!!.toInt(),
-                        hostScore = hostScore!!.toInt(),
-                    )
-                    val today = Date()
-                    if (today.compareTo(game.date) > 0)
-                        game.status = GameStatus.END
-                    else
-                        game.status = GameStatus.NOT_YET_START
-                    games.add(game)
-                    getTeamById(game.hostTeam)?.gamesIdList?.add(game.gameId)
-                    getTeamById(game.guestTeam)?.gamesIdList?.add(game.gameId)
+            val allGamesURL: ArrayList<String> = arrayListOf(
+                "https://pleagueofficial.com/schedule-regular-season/2021-22",
+                "https://pleagueofficial.com/schedule-playoffs/2021-22",
+                "https://pleagueofficial.com/schedule-finals/2021-22"
+            )
 
-                }
+            for (url in allGamesURL) {
+                val doc =
+                    Jsoup.connect(url).get()
+                doc.select("div.col-lg-12.col-12")
+                    .parallelStream()
+                    .filter { it != null }
+                    .forEach {
+                        val regex =
+                            "^([0-9][0-9])/([0-9][0-9]) \\(.*?\\) ([0-9][0-9]:[0-9][0-9]) 客隊 (?:\\S+) (.*?) ([0-9]*?) [0-9]*? [A-Za-z]*([0-9]*) (.*?) 追蹤賽事 (.*? / .*?) ([0-9]*?) [0-9]*? 主隊 (?:\\S+) (.*?) 數據 售票 (?:.*? / .*?)\$".toRegex()
+
+                        val parsed = regex.find(it.text())
+                        val month = parsed?.groups?.get(1)?.value
+                        val date = parsed?.groups?.get(2)?.value
+                        val time = parsed?.groups?.get(3)?.value
+                        val guest = parsed?.groups?.get(4)?.value
+                        val guestScore = parsed?.groups?.get(5)?.value
+                        val id = parsed?.groups?.get(6)?.value
+                        val location = parsed?.groups?.get(7)?.value
+                        val audience = parsed?.groups?.get(8)?.value
+                        val hostScore = parsed?.groups?.get(9)?.value
+                        val host = parsed?.groups?.get(10)?.value
+                        var game = Game(
+                            gameId = GameID(id!!.toInt() + 72),
+                            date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse("2022-$month-${date}T${time}:00Z"),
+                            guestTeam = getTeamIdByName(guest!!),
+                            hostTeam = getTeamIdByName(host!!),
+                            guestScore = guestScore!!.toInt(),
+                            hostScore = hostScore!!.toInt(),
+                        )
+
+                        val today = Date()
+                        if (today.compareTo(game.date) > 0)
+                            game.status = GameStatus.END
+                        else
+                            game.status = GameStatus.NOT_YET_START
+                        games.add(game)
+                        getTeamById(game.hostTeam)?.gamesIdList?.add(game.gameId)
+                        getTeamById(game.guestTeam)?.gamesIdList?.add(game.gameId)
+                    }
+            }
 ////                Only the original thread that created a view hierarchy can touch its views.
 //                gamesFrag.updateGameCardView()
-
             true
         } catch (e: Exception) {
             false
@@ -182,14 +190,14 @@ class MainActivity : AppCompatActivity() {
                     val regex =
                         "^([0-9]) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (?:.*)\$".toRegex()
                     val parsed = regex.find(it.text())
-                    var ranking= parsed?.groups?.get(1)?.value
-                    val teamName= parsed?.groups?.get(2)?.value
-                    val total= parsed?.groups?.get(3)?.value
-                    val win= parsed?.groups?.get(4)?.value
-                    val lose= parsed?.groups?.get(5)?.value
-                    val gb= parsed?.groups?.get(7)?.value
-                    val streakL= parsed?.groups?.get(8)?.value
-                    val streakN= parsed?.groups?.get(9)?.value
+                    var ranking = parsed?.groups?.get(1)?.value
+                    val teamName = parsed?.groups?.get(2)?.value
+                    val total = parsed?.groups?.get(3)?.value
+                    val win = parsed?.groups?.get(4)?.value
+                    val lose = parsed?.groups?.get(5)?.value
+                    val gb = parsed?.groups?.get(7)?.value
+                    val streakL = parsed?.groups?.get(8)?.value
+                    val streakN = parsed?.groups?.get(9)?.value
 
                     val teamID = teamName?.let { it1 -> getTeamIdByName(it1) }
                     val team = getTeamById(teamID)
@@ -197,7 +205,7 @@ class MainActivity : AppCompatActivity() {
                     team?.ranking = ranking!!
 
                     team?.totalRecord = Record(win!!.toInt(), lose!!.toInt())
-                    team?.streak = streakL!!+streakN!!
+                    team?.streak = streakL!! + streakN!!
 
                     team?.gamesBack = gb!!
                 }
